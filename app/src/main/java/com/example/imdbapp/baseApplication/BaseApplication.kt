9e5a4e0874f555442c29776a4d1abf44288aab12
AliShiri <Your new email>
2022-07-24 12:52:01 -0700

    add BaseApplication

---
+++ app/src/main/java/com/example/imdbapp/baseApplication/BaseApplication.kt
@@ -0,0 +1,15 @@
+package com.example.imdbapp.baseApplication
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
