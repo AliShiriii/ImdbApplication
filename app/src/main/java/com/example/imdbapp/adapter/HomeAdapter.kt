a5efda80684502d47c7cf9bc6b46ec21a42cd13b
AliShiri <Your new email>
2022-07-25 08:39:09 -0700

    add BatmanModel in package model

---
+++ app/src/main/java/com/example/imdbapp/adapter/HomeAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.imdbapp.adapter
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.imdbapp.databinding.HomeItemBinding
+
+class HomeAdapter(private val context: Context) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    class HomeViewHolder(val binding: HomeItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/imdbapp/model/BatmanModel.kt
@@ -0,0 +1,7 @@
+package com.example.imdbapp.model
+
+data class BatmanModel(
+    val Response: String,
+    val Search: List<Search>,
+    val totalResults: String
+)
